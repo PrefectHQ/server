cac3cfe3945236e138514258cd8192619f8d92cc
Josh Meek <josh@prefect.io>
2020-09-21 15:20:15 -0400

    Add migration for flow run config column

---
+++ services/postgres/alembic/versions/2020-09-21T145318_add_flow_run_config.py
@@ -0,0 +1,26 @@
+"""
+Add flow run config
+
+Revision ID: 850b76d44332
+Revises: 9cb7539b7363
+Create Date: 2020-09-21 14:53:18.321017
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+# revision identifiers, used by Alembic.
+revision = '850b76d44332'
+down_revision = '9cb7539b7363'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("flow", sa.Column("run_config", JSONB, nullable=False, server_default="{}"))
+
+
+def downgrade():
+    op.drop_column("flow", "run_config")
