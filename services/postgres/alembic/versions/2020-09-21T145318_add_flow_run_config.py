fa0c496997b0c48754f6b33a87a34171e3a116af
Josh Meek <josh@prefect.io>
2020-09-21 15:31:32 -0400

    Retrieve run config labels in get_runs_in_queue

--- services/postgres/alembic/versions/2020-09-21T145318_add_flow_run_config.py
+++ services/postgres/alembic/versions/2020-09-21T145318_add_flow_run_config.py
@@ -12,14 +12,16 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 
 # revision identifiers, used by Alembic.
-revision = '850b76d44332'
-down_revision = '9cb7539b7363'
+revision = "850b76d44332"
+down_revision = "9cb7539b7363"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column("flow", sa.Column("run_config", JSONB, nullable=False, server_default="{}"))
+    op.add_column(
+        "flow", sa.Column("run_config", JSONB, nullable=False, server_default="{}")
+    )
 
 
 def downgrade():
