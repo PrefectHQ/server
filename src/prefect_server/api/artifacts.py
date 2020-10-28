e9c96e758104b74e1a3ec4d499d78833ba73e8d6
Josh Meek <josh@prefect.io>
2020-10-28 13:49:42 -0400

    Working artifacts API POC

--- src/prefect_server/api/artifacts.py
+++ src/prefect_server/api/artifacts.py
@@ -9,7 +9,19 @@ async def create_task_run_artifact(
     data: dict,
     tenant_id: str = None,
 ) -> str:
-    """"""
+    """
+    Create a task run artifact.
+
+    Args:
+        - task_run_id (str): the task run id
+        - kind (str): the artifact kind
+        - data (dict): the artifact data
+        - tenant_id (str, optional): the tenant id that this artifact belongs to. Defaults
+            to the tenant ID linked to the task run
+
+    Returns:
+        - str: the task run artifact ID
+    """
     task_run = await models.TaskRun.where(id=task_run_id).first({"tenant_id"})
 
     if not task_run:
@@ -22,3 +34,47 @@ async def create_task_run_artifact(
     return await models.TaskRunArtifact(
         tenant_id=tenant_id, task_run_id=task_run_id, kind=kind, data=data
     ).insert()
+
+
+@register_api("artifacts.update_task_run_artifact")
+async def update_task_run_artifact(
+    task_run_artifact_id: str,
+    data: dict,
+) -> bool:
+    """
+    Update a task run artifact.
+
+    Args:
+        - task_run_artifact_id (str): the task run artifact id
+        - data (dict): the artifact data
+
+    Returns:
+        - bool: if the update was successful
+    """
+    if not task_run_artifact_id:
+        raise ValueError("Must supply a valid task run artifac ID to update")
+
+    result = await models.TaskRunArtifact.where(id=task_run_artifact_id).update(
+        set={"data": data}
+    )
+    return bool(result.affected_rows)  # type: ignore
+
+
+@register_api("artifacts.delete_task_run_artifact")
+async def delete_task_run_artifact(
+    task_run_artifact_id: str,
+) -> bool:
+    """
+    Delete a task run artifact.
+
+    Args:
+        - task_run_artifact_id (str): the task run artifact id
+
+    Returns:
+        - bool: if the deletion was successful
+    """
+    if not task_run_artifact_id:
+        raise ValueError("Must supply a valid task run artifac ID to delete")
+
+    result = await models.TaskRunArtifact.where(id=task_run_artifact_id).delete()
+    return bool(result.affected_rows)  # type: ignore
--- src/prefect_server/graphql/artifacts.py
+++ src/prefect_server/graphql/artifacts.py
@@ -18,3 +18,22 @@ async def resolve_create_task_run_artifact(
         tenant_id=input.get("tenant_id"),
     )
     return {"id": task_run_artifact_id}
+
+@mutation.field("update_task_run_artifact")
+async def resolve_update_task_run_artifact(
+    obj: Any, info: GraphQLResolveInfo, input: dict
+) -> dict:
+    success = await api.artifacts.update_task_run_artifact(
+        task_run_artifact_id=input.get("task_run_artifact_id"),
+        data=input.get("data"),
+    )
+    return {"success": success}
+
+@mutation.field("delete_task_run_artifact")
+async def resolve_delete_task_run_artifact(
+    obj: Any, info: GraphQLResolveInfo, input: dict
+) -> dict:
+    success = await api.artifacts.delete_task_run_artifact(
+        task_run_artifact_id=input.get("task_run_artifact_id"),
+    )
+    return {"success": success}
