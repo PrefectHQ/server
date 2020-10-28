7e47b8204f5d8174548ba9f16a38fb6a9eaf8ed1
Michael Adkins <michael@prefect.io>
2020-10-28 11:01:52 -0500

    Remove extra note

--- src/prefect_server/api/flows.py
+++ src/prefect_server/api/flows.py
@@ -192,11 +192,8 @@ async def create_flow(
         }
     ).first({"id", "schedule", "settings"})
 
-    # create the flow group or check for the idempotency key in the existing group
-    # Note: The key is stashed in the `settings` dict of the flow group which makes
-    # idempotency only apply to the most recent flow creation. If we want to avoid
-    # re-registering old flow versions we'll need to add a column to the Flow table
-    # TODO: Should we leave empty idempotency_keys as `None` or generate a uuid4?
+    # create the flow group or check for the idempotency key in the existing flow group
+    # settings
     if flow_group is None:
         flow_group_id = await models.FlowGroup(
             tenant_id=tenant_id,
