67aeb3db00356f991622c5d35b34650356100179
Chris White <chris@prefect.io>
2021-02-08 12:58:10 -0800

    Use list comprehension instead, and import asyncio

--- src/prefect_server/api/flows.py
+++ src/prefect_server/api/flows.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 import hashlib
 import json
@@ -372,10 +373,12 @@ async def archive_flow(flow_id: str) -> bool:
         }
     ).get({"id"})
     msg = f"Flow {flow_id} was archived."
-    tasks = []
-    for flow_run in fr_ids:
-        tasks.append(api.states.cancel_flow_run(flow_run.id, state_message=msg))
-    await asyncio.gather(*tasks)
+    await asyncio.gather(
+        *[
+            api.states.cancel_flow_run(flow_run.id, state_message=msg)
+            for flow_run in fr_ids
+        ]
+    )
     return True
 
 
