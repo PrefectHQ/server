56bb52731258e70b364c4264d84b0505a947f481
Peter Roelants <peter.roelants@gmail.com>
2021-03-15 16:39:33 +0100

    Access level logging

--- src/prefect_server/services/graphql/server.py
+++ src/prefect_server/services/graphql/server.py
@@ -52,7 +52,7 @@ def health(request: Request) -> JSONResponse:
 
 
 def _get_uvicorn_log_level():
-    prefect_log_level = prefect_server.configuration.config["logging"]["level"]
+    prefect_log_level = prefect_server.configuration.config.logging.level
     # Uvicorn log levels are lower case
     uvicorn_log_level = prefect_log_level.lower()
     if uvicorn_log_level in uvicorn.config.LOG_LEVELS:
@@ -71,4 +71,5 @@ if __name__ == "__main__":
         host=prefect_server.config.services.graphql.host,
         port=prefect_server.config.services.graphql.port,
         log_level=_get_uvicorn_log_level(),
+        access_log=not prefect_server.configuration.config.services.graphql.disable_access_logs,
     )
