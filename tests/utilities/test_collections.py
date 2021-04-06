916aa1fd23f3694f833df75b3b3f2cf89521b1ec
Zach Angell <zachary.james.angell@gmail.com>
2021-04-06 14:31:51 -0400

    Rename test method

--- tests/utilities/test_collections.py
+++ tests/utilities/test_collections.py
@@ -1,12 +1,12 @@
 from prefect_server.utilities.collections import chunked_iterable
 
 
-def test_get_chunk_of_list():
+def test_chunked_iterable_of_list():
     chunks = [chunk for chunk in chunked_iterable(list(range(10)), 4)]
     expected_chunks = [(0, 1, 2, 3), (4, 5, 6, 7), (8, 9)]
     assert chunks == expected_chunks
 
 
-def test_get_chunk_of_empty_iterable():
+def test_chunked_iterable_of_empty_iterable():
     chunks = [chunk for chunk in chunked_iterable([], 4)]
     assert len(chunks) == 0
