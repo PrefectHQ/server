fc66b339717d8234d55275e37d47f28e86c2d215
Bouke Krom <bouke.krom@sensorfact.nl>
2021-08-31 18:36:36 +0200

    Add unit test for configuration parameter

--- tests/services/test_scheduler.py
+++ tests/services/test_scheduler.py
@@ -3,6 +3,7 @@ from prefect import api
 from prefect import models as m
 
 from prefect_server.services.towel.scheduler import Scheduler
+from prefect_server import config
 
 
 @pytest.fixture(autouse=True)
@@ -28,3 +29,8 @@ async def test_scheduler_does_not_run_for_archived_flows(flow_id):
 async def test_scheduler_does_not_run_for_flows_with_inactive_schedules(flow_id):
     await api.flows.set_schedule_inactive(flow_id=flow_id)
     assert await Scheduler().run_once() == 0
+
+
+async def test_scheduler_creates_amount_configured_runs():
+    config.services.towel.max_scheduled_runs_per_flow = 100
+    assert await Scheduler().run_once() == 100
